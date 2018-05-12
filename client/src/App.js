2ea39dc52befb8997b8452a016a037ed0e7ff6a9
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2018-05-12 23:28:34 +0300

    Added a skeleton of a web client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import VoteView from './View/VoteView'
+
+class App extends Component {
+  render() {
+    return (
+        <div className='App'>
+            <VoteView />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/View/VoteView.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HomeView extends Component {
+  render() {
+    return <p>Vote View</p>;
+  }
+}
+
+export default HomeView;
