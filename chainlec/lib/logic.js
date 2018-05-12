b90a58a196e9f2a3eea31ebe42b7b7eca68ddd96
Rostyslav_Stakhiv <rostyk96@gmail.com>
2018-05-12 23:22:00 +0300

    - Working on business network data model and logic

--- chainlec/lib/logic.js
+++ chainlec/lib/logic.js
@@ -1,23 +1,35 @@
 'use strict';
 /**
- * Write your transction processor functions here
- */
-
-/**
- * Sample transaction
- * @param {org.acme.chainlec.ChangeAssetValue} changeAssetValue
- * @transaction
- */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.acme.chainlec.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
-        });
-}
+  * give a vote to candidate
+  * @param {org.acme.chainlec} Vote - the Vote to be processed
+  * @transaction
+  */
+ async function Vote(vote) {
+    // get voter
+    var voter = vote.voter;
+    // get candidate
+    var candidate = vote.candidate;
+    // if voter already voted
+    var poll = vote.poll;
+    var voters = poll.voters;
+    if(voters.indexOf(voter) > -1) {
+      throw new Error('Already voted');
+    }
+    // if voter has not voted give a vote to candidate
+    candidate.numberOfVotes += 1;
+    poll.voters.push(voter);
+    // get asset 'org.acme.votenetwork.Candidate'
+    return getAssetRegistry('org.acme.votenetwork.Candidate').then(function(CandidateRegistry) {
+      // Get participant 'org.acme.votenetwork.Voter'
+      getParticipantRegistry('org.acme.votenetwork.Voter').then(function(participantRegistry) {
+        // Modify the properties of the voter .
+        voter.voted = true;
+        // Update the voter in the participant registry.
+        return participantRegistry.update(voter);
+      }).catch(function(error) {
+        // Add optional error handling here.
+      });
+      // update the candidate registry
+      return CandidateRegistry.update(candidate);
+    });
+  }
